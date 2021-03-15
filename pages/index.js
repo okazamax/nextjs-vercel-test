c5819cddb33681858c20fc916be8378e32e8b6d7
Max <49245854+okazamax@users.noreply.github.com>
2021-03-14 17:28:09 -0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -15,8 +15,7 @@ export default function Home() {
         </h1>
 
         <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
+          My name is Max!
         </p>
 
         <div className={styles.grid}>
